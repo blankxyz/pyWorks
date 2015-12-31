9d7edac9db69e88692c0d5d3f91e0b5d9778895c
bigbangbang <songyq0509@163.com>
2015-12-31 15:45:17 +0800

    2015-12-31@baicmotor

--- microblog-0.2/app/static/highcharts.js
+++ microblog-0.2/app/static/highcharts.js
@@ -573,9 +573,9 @@
             snap: Mb ? 25 : 10,
             style: {color: "#333333", cursor: "default", fontSize: "12px", padding: "8px", pointerEvents: "none", whiteSpace: "nowrap"}
         }, credits: {
-            enabled: !0, text: "Highcharts.com", href: "http://www.highcharts.com", position: {align: "right", x: -10, verticalAlign: "bottom", y: -5}, style: {
+            enabled: !0, text: "bugzilla", href: "http://10.14.70.94/bugzilla/index.cgi", position: {align: "right", x: -10, verticalAlign: "bottom", y: -5}, style: {
                 cursor: "pointer", color: "#909090",
-                fontSize: "9px"
+                fontSize: "30px"
             }
         }
     };
--- microblog-0.2/app/views.py
+++ microblog-0.2/app/views.py
@@ -21,29 +21,11 @@ def dayReport():
     reports = db.daysTotalByMember(days)
     return render_template("dayReport.html", title=title, reportDays=days, reports=json.loads(reports))
 
-
 @app.route('/add')
 def add_numbers():
     a = request.args.get('a', 0, type=int)
     b = request.args.get('b', 0, type=int)
     return jsonify(result=a + b)
-
-
-@app.route('/log')
-def log():
-    d = {'1.htldxhzj.duapp.com': 9398,
-         'gtxapi.cdn.duapp.com': 79496,
-         'www.xxx.com': 2477070,
-         'www.baidu.com': 1465,
-         'www.bing.com': 777,
-         'www.aaa.com': 1113101,
-         'www.ccc.net.cn': 922,
-         'www.zhanimei.ga': 29847,
-         'www.zhanimei.ml': 40155,
-         'www.zhasini.ml': 373436}
-    categories = d.keys()
-    data = d.values()
-    return render_template('log.html', categories=categories, data=data)
 
 @app.route('/')
 @app.route('/getComment')
@@ -56,4 +38,4 @@ def getComment():
     print when
     print result
     #return jsonify(result)
-    return render_template('ajaxTest.html')
+    return render_template('ajaxTest.html',bugId=bugId,when=when)
