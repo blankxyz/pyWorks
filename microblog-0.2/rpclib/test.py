8b474491bd12830c04de37d2e4f25e635b77b727
sougannkyou <songyq0509@163.com>
2016-01-14 14:38:32 +0800

    2016-01-14@baicmotor

--- microblog-0.2/rpclib/test.py
+++
@@ -1,7 +0,0 @@
-from suds.client import Client
-from suds.transport.https import HttpAuthenticated
-
-t = HttpAuthenticated(username='songyq@ehr.com', password='314159')
-test = Client('http://127.0.0.1:8080/WebService/ws/DeviceManageService?wsdl', transport=t)
-
-print test
