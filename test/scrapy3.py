3de6be751b427af8b36a577f481367836b267b0a
sougannkyou <songyq0509@163.com>
2016-02-25 15:29:36 +0900

    Update scrapy3.py

--- test/scrapy3.py
+++ test/scrapy3.py
@@ -1,93 +1,71 @@
 # -*- coding: utf-8 -*-
-
-
 import urllib2
 import cookielib
 import urllib
 import Image
 import cStringIO
-from pytesser import *
+from pytesser.pytesser import *
 import re
 import os
 
-# 避免 UnicodeEncodeError: 'ascii' codec can't encode character.  的报错
+# UnicodeEncodeError: 'ascii' codec can't encode character.
 import sys
-
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-cookiejar = cookielib.MozillaCookieJar()
-# 将一个保存cookie对象，和一个HTTP的cookie的处理器绑定
-cookieSupport = urllib2.HTTPCookieProcessor(cookiejar)
-# 下面两行为了调试的
+cookieJar = cookielib.MozillaCookieJar()
+cookieSupport = urllib2.HTTPCookieProcessor(cookieJar)
+# 调试用
 httpHandler = urllib2.HTTPHandler(debuglevel=1)
 httpsHandler = urllib2.HTTPSHandler(debuglevel=1)
-# 创建一个opener，将保存了cookie的http处理器，还有设置一个handler用于处理http的
 opener = urllib2.build_opener(cookieSupport, httpsHandler)
-# 将包含了cookie、http处理器、http的handler的资源和urllib2对象绑定在一起，安装opener,此后调用urlopen()时都会使用安装过的opener对象，
 urllib2.install_opener(opener)
 
-# 登陆页面
-loginpage = "http://zhuzhou2013.feixuelixm.teacher.com.cn/IndexPage/Index.aspx"
+loginPage = "http://zhuzhou2013.feixuelixm.teacher.com.cn/IndexPage/Index.aspx"
 # 要post的url
 LoginUrl = "http://zhuzhou2013.feixuelixm.teacher.com.cn/GuoPeiAdmin/Login/Login.aspx"
 
-##打开登陆页面, 以此来获取cookies   。  但是因为  ##打开验证码页面就可以获取全部cookies了，所以可以直接跳过这一步。算是可有可无的
-# taobao = urllib2.urlopen(loginpage)
-##打印cookies
-# print   cookiejar
-##先打开页面获取的cookie与  后打开验证码页面的cookie不同。
-
-##提取验证码text(手动输入验证码)
-# vrifycodeUrl = "http://zhuzhou2013.feixuelixm.teacher.com.cn/GuoPeiAdmin/Login/ImageLog.aspx"
-# file = urllib2.urlopen(vrifycodeUrl)
+# 手动输入验证码
+# verifyCodeUrl = "http://zhuzhou2013.feixuelixm.teacher.com.cn/GuoPeiAdmin/Login/ImageLog.aspx"
+# file = urllib2.urlopen(verifyCodeUrl)
 # pic= file.read()
-# path = "c:code.jpg"
-##img = cStringIO.StringIO(file) # constructs a StringIO holding the image  AttributeError: addinfourl instance has no attribute 'seek'
-# localpic = open(path,"wb")
-# localpic.write(pic)
-# localpic.close()
+# path = "code.jpg"
+# #img = cStringIO.StringIO(file) # constructs a StringIO holding the image  AttributeError: addinfourl instance has no attribute 'seek'
+# localPic = open(path,"wb")
+# localPic.write(pic)
+# localPic.close()
 # print "please  %s,open code.jpg"%path
-##text =raw_input("input code :")
+# text =raw_input("input code :")
 # im = Image.open(path)
 # text =image_to_string(im)
 # print text
 
-
-# 提取验证码地址(用pytesser 识别， 自己网上找教程安装)
-# 并且用pytesser 识别验证码，赋值给 text ，并打印出来。
-vrifycodeUrl = "http://zhuzhou2013.feixuelixm.teacher.com.cn/GuoPeiAdmin/Login/ImageLog.aspx"
-file = urllib2.urlopen(vrifycodeUrl).read()
-img = cStringIO.StringIO(
-    file)  # constructs a StringIO holding the image  AttributeError: addinfourl instance has no attribute 'seek'
+# using pytesser to passer the verify code。
+verifyCodeUrl = "http://zhuzhou2013.feixuelixm.teacher.com.cn/GuoPeiAdmin/Login/ImageLog.aspx"
+file = urllib2.urlopen(verifyCodeUrl).read()
+img = cStringIO.StringIO(file)  # constructs a StringIO holding the image  AttributeError: addinfourl instance has no attribute 'seek'
 im = Image.open(img)
 text = image_to_string(im)
-print "vrifycode:", text
+print "verifyCodeUrl:", text
 
-# 设置cookie的值，因为post request head  需要 返回 cookie (不是cookies ，是将cookies的格式处理后的值)
 cookies = ''
-# 这里要从
-for index, cookie in enumerate(cookiejar):
+for index, cookie in enumerate(cookieJar):
     # print '[',index, ']';
     # print cookie.name;
     # print cookie.value;
     # print "###########################"
-    cookies = cookies + cookie.name + "=" + cookie.value + ";";
+    cookies = cookies + cookie.name + "=" + cookie.value + ";"
 print "###########################"
 cookie = cookies[:-1]
 print "cookies:", cookie
 
-# 用户名，密码
-# 当然，我这里登顶要处理掉密码和用户名
 # username = "7879954564555664"
 # password = "12313164"
 
-
-# 用户名，密码
 username = "430223198809308045"
 password = "56961888"
 
-# 请求数据包
+# post date: copy from httpFox
 postData = {
     '__EVENTTARGET': '',
     '__EVENTARGUMENT': '',
@@ -100,30 +78,26 @@ postData = {
     'ClientScreenWidth': 1180
 }
 
-# post请求头部
+# post header copy: from httpFox
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-cn,en-us;q=0.8,zh;q=0.5,en;q=0.3',
     'Accept-Encoding': 'gzip, deflate',
-
     'Host': 'zhuzhou2013.feixuelixm.teacher.com.cn',
     'Cookie': cookies,
     'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:29.0) Gecko/20100101 Firefox/29.0',
     'Referer': 'http://zhuzhou2013.feixuelixm.teacher.com.cn/GuoPeiAdmin/Login/Login.aspx',
-    # 'Content-Type': 'application/x-www-form-urlencoded',
-    # 'Content-Length' :474,
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length' :473,
     'Connection': 'Keep-Alive'
 }
 
 # 合成post数据
 data = urllib.urlencode(postData)
-print "data:###############"
-print  data
-# 创建request
-# 构造request请求
+print "post data:", data
+
 request = urllib2.Request(LoginUrl, data, headers)
 try:
-    # 访问页面
     response = urllib2.urlopen(request)
     # cur_url =  response.geturl()
     # print "cur_url:",cur_url
@@ -138,15 +112,13 @@ f = response.read().decode("utf8")
 outfile = open("rel_ip.txt", "w")
 print >> outfile, "%s" % (f)
 
-# 但因响应的信息
 info = response.info()
 print info
 
-# 测试登陆是否成功，因为在testurl只有登陆后才能访问
-testurl = "http://zhuzhou2013.feixuelixm.teacher.com.cn/GuoPeiAdmin/Login/LoginedPage.aspx"
-
+# test for login success,if LoginedPage.aspx are loaded.
+testUrl = "http://zhuzhou2013.feixuelixm.teacher.com.cn/GuoPeiAdmin/Login/LoginedPage.aspx"
 try:
-    response = urllib2.urlopen(testurl)
+    response = urllib2.urlopen(testUrl)
 except  urllib2.HTTPError, e:
     print e.code
 
@@ -158,10 +130,8 @@ print >> outfile, "%s" % (f)
 # 在返回的网页中，查找“你好” 两个字符，因为只有登陆成功后才有两个字，找到了即表示登陆成功。建议用英文
 tag = '你好'.encode("utf8")
 if re.search(tag, f):
-    # 登陆成功
     print 'Logged in successfully!'
 else:
-    # 登陆失败
     print 'Logged in failed, check result.html file for details'
 
 response.close()
