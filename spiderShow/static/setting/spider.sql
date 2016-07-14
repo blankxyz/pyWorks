8fc541e3d64aab2021ef686f0211f8566ce62d3a
song <songyq0509@163.com>
2016-07-15 00:34:02 +0800

    Merge remote-tracking branch 'origin/master'

--- spiderShow/static/setting/spider.sql
+++ spiderShow/static/setting/spider.sql
@@ -47,3 +47,35 @@ CREATE TABLE `url_rule` (
 #
 
 INSERT INTO `url_rule` VALUES (1,'http://bbs.tianya.cn/','bbs.tianya.cn',0,b'0',0,'index',NULL,'{\'index\':index}');
+
+
+#-------------------------------------
+CREATE TABLE content_rule
+(
+    Id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    item VARCHAR(255) DEFAULT 'title' NOT NULL COMMENT 'title，ctime，gtime，content 等',
+    rule VARCHAR(255) COMMENT '匹配规则',
+    etc VARCHAR(255) COMMENT '备注'
+);
+CREATE INDEX item ON content_rule (item);
+CREATE TABLE url_rule
+(
+    Id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    start_urls VARCHAR(255) DEFAULT '' NOT NULL COMMENT 'homepage',
+    site_domain VARCHAR(255) DEFAULT '' NOT NULL COMMENT 'domain',
+    scope INT(1) DEFAULT '0' NOT NULL COMMENT '0:netloc,1:path,2:query',
+    yes_no BIT(1) DEFAULT b'0' NOT NULL COMMENT '0:white,1:black',
+    weight INT(1) DEFAULT '0' NOT NULL COMMENT '0:高，1：中，2：低',
+    regex VARCHAR(255) COMMENT '正则表达式',
+    etc VARCHAR(255) COMMENT '备注',
+    setting_json LONGTEXT NOT NULL COMMENT 'json版 setting'
+);
+CREATE INDEX scope ON url_rule (start_urls, site_domain);
+CREATE TABLE current_main_setting
+(
+    start_url VARCHAR(255) PRIMARY KEY NOT NULL,
+    site_domain VARCHAR(255) NOT NULL,
+    setting_json LONGTEXT
+);
+CREATE UNIQUE INDEX current_setting_site_domain_uindex ON current_main_setting (site_domain);
+CREATE UNIQUE INDEX current_setting_start_url_uindex ON current_main_setting (start_url);
