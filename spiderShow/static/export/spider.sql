6a05ad6b07054e1cbe7b402e4a8ba0f7877f0cdf
songyq0509@163.com <syq314159>
2016-07-22 19:22:38 +0800

    Merge remote-tracking branch 'origin/master'

--- spiderShow/static/export/spider.sql
+++ spiderShow/static/export/spider.sql
@@ -1,5 +1,5 @@
 ﻿# Host: 127.0.0.1  (Version: 5.6.20)
-# Date: 2016-07-20 11:06:58
+# Date: 2016-07-22 19:22:17
 # Generator: MySQL-Front 5.3  (Build 4.214)
 
 /*!40101 SET NAMES utf8 */;
@@ -30,18 +30,19 @@ CREATE TABLE `content_rule` (
 DROP TABLE IF EXISTS `current_domain_setting`;
 CREATE TABLE `current_domain_setting` (
   `Id` int(11) NOT NULL DEFAULT '0',
+  `user` varchar(255) DEFAULT NULL,
   `start_url` varchar(255) DEFAULT NULL,
   `site_domain` varchar(255) DEFAULT NULL,
   `black_domain` varchar(255) DEFAULT NULL COMMENT '以 ; 分割的列表',
   `setting_json` longtext COMMENT '配置简报，json版',
   PRIMARY KEY (`Id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='初始化配置，只有一条当前配置记录。';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='初始化配置，一个user只有一条当前配置记录。';
 
 #
 # Data for table "current_domain_setting"
 #
 
-INSERT INTO `current_domain_setting` VALUES (0,'http://cpt.xtu.edu.cn/','cpt.xtu.edu.cn','','{\"detail_regex_save_list\": [], \"site_domain\": \"cpt.xtu.edu.cn\", \"list_regex_save_list\": [[\"\\/$\", \"0\"]], \"start_url\": \"http://cpt.xtu.edu.cn/\"}');
+INSERT INTO `current_domain_setting` VALUES (0,'admin','http://bbs.tianya.cn','bbs.tianya.cn','','');
 
 #
 # Structure for table "url_rule"
@@ -50,6 +51,7 @@ INSERT INTO `current_domain_setting` VALUES (0,'http://cpt.xtu.edu.cn/','cpt.xtu
 DROP TABLE IF EXISTS `url_rule`;
 CREATE TABLE `url_rule` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL DEFAULT 'admin' COMMENT 'user',
   `start_url` varchar(255) NOT NULL DEFAULT '' COMMENT 'homepage',
   `site_domain` varchar(255) NOT NULL DEFAULT '' COMMENT 'domain',
   `detail_or_list` char(1) DEFAULT '0' COMMENT '0:detail,1:list',
@@ -60,10 +62,30 @@ CREATE TABLE `url_rule` (
   `etc` varchar(255) DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`Id`),
   KEY `scope` (`start_url`,`site_domain`)
-) ENGINE=InnoDB AUTO_INCREMENT=216 DEFAULT CHARSET=utf8 COMMENT='url匹配规则';
+) ENGINE=InnoDB AUTO_INCREMENT=261 DEFAULT CHARSET=utf8 COMMENT='url匹配规则';
 
 #
 # Data for table "url_rule"
 #
 
-INSERT INTO `url_rule` VALUES (215,'http://cpt.xtu.edu.cn/','cpt.xtu.edu.cn','1','1','0','0','/$',NULL);
+INSERT INTO `url_rule` VALUES (256,'admin','http://bbs.tianya.cn','bbs.tianya.cn','0','1','0','0','post-',NULL),(257,'admin','http://bbs.tianya.cn','bbs.tianya.cn','0','1','0','0','pic-',NULL),(258,'admin','http://bbs.tianya.cn','bbs.tianya.cn','1','1','0','0','/$',NULL),(259,'admin','http://bbs.tianya.cn','bbs.tianya.cn','1','1','0','0','list',NULL),(260,'admin','http://bbs.tianya.cn','bbs.tianya.cn','1','1','0','0','index',NULL);
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `Authorization` char(1) DEFAULT NULL COMMENT '0:admin,1:normal',
+  `etc` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+INSERT INTO `user` VALUES (1,'admin','admin','0','admin'),(2,'user','user','1','normal user');
