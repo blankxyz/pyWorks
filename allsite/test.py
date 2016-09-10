7d95bc5dedac127d4eae68bee7bcc2e3fb214801
song <songyq0509@163.com>
2016-09-10 23:17:01 +0800

    Merge remote-tracking branch 'origin/master'

--- allsite/test.py
+++ allsite/test.py
@@ -1,61 +1,102 @@
-# coding:utf-8
-import urllib
-from bs4 import BeautifulSoup
-import re
+# coding=utf-8
+import time
+from selenium import webdriver
+
+class PageEyes(object):
+    def __init__(self):
+        self._loadJsFmt = """
+            var script = document.createElement('script');
+            script.src = "{0}";
+            document.body.appendChild(script);
+        """
+        self._jquery_cdn = "http://cdn.bootcss.com/jquery/2.2.3/jquery.min.js"
+
+        self._browse_window_width = 800
+        self._browse_window_high = 600
+
+        self._space_window_rate = 0.1
+        self._window_width = 400
+        self._window_high = 200
+
+        self._max_width = 800  # js 加载后从截图得到
+        self._max_high = 600  # js 加载后从截图得到
+        self._space_line_rate = 0.2
+        self._Nx = 5  # 采样倍率
+        self._Ny = 5
+        self._image_handle = None
 
 
-def get_all_url(url):
-    urls = []
-    web = urllib.urlopen(url)
+    def is_jqueryLoaded(self, driver):
+        try:
+            loaded = driver.executeScript("return " + "jQuery()!=null")
+        except Exception:
+            loaded = None
+        return loaded
+
+    def capture(self, url, capture_img):
+        # system.setProperty("webdriver.chrome.driver",
+        #                    "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome");
+        driver = webdriver.Chrome()
+        driver.set_window_size(self._browse_window_width, self._browse_window_high)
+        # driver.maximize_window()
+        driver.implicitly_wait(1)
+        driver.get(url)
+        # driver.execute_script("""
+        #     (function () {
+        #       var y = 0;
+        #       var step = 100;
+        #       window.scroll(0, 0);
+        #
+        #       function f() {
+        #         if (y < document.body.scrollHeight) {
+        #           y += step;
+        #           window.scroll(0, y);
+        #           setTimeout(f, 50);
+        #         } else {
+        #           window.scroll(0, 0);
+        #           document.title += "scroll-done";
+        #         }
+        #       }
+        #       setTimeout(f, 1000);
+        #     })();
+        #   """)
+        # for i in xrange(30):
+        #     if "scroll-done" in driver.title:
+        #         break
+        #     time.sleep(1)
+        if self.is_jqueryLoaded(driver) is None:  # inject jquery
+            driver.execute_script(self._loadJsFmt.format(self._jquery_cdn))
+
+        js = '''
+            var patrn_rubbish = /uid|username|space|search|blog|group/;
+            var patrn_detail = /post|thread|detail/;
+            var patrn_list = /list|index|forum|fid/;
+            $("embed").removeAttr("src")
+            $(document).ready(function(){
+                $("a").css({"borderColor":"blue","color":"red"});
+                $("a").each(function(){
+                    $(this).children("img").removeAttr("src").css("background","black");
+                    if($(this).siblings("a").length>3){
+                        // alert($(this).siblings("a").length);
+                        $(this).parent("div").css({"background":"black","color":"black"});
+                    }
+                });
+            });
+            '''
+        driver.execute_script(js)
+
+        # screen capture
+        driver.get_screenshot_as_file(capture_img)
+        driver.close()
 
 
-    soup = BeautifulSoup(web.read())
-    tags_a = soup.findAll(name='a', attrs={'href': re.compile("^https?://")})
-    try:
-        for tag_a in tags_a:
-            urls.append(tag_a['href'])
-            # return urls
-    except:
-        pass
-    return urls
+if __name__ == "__main__":
+    start = time.clock()
+    url = 'http://bbs.tianya.cn/'  # link full
+    # url = 'http://zx.jiaju.sina.com.cn/2011571/'  # 装修 list imageful
+    # url = 'http://sports.sina.com.cn/basketball/nba/2016-05-23/doc-ifxsktkr5904181.shtml'  # sport detail
+    # url = 'http://news.sina.com.cn/'
+    capture_img = 'test.png'
 
-def get_local_urls(url):
-    local_urls = []
-    urls = get_all_url(url)
-    for _url in urls:
-        ret = _url
-        if 'freebuf.com' in ret.replace('//', '').split('/')[0]:
-            local_urls.append(_url)
-    return local_urls
-
-
-def get_remote_urls(url):
-    remote_urls = []
-    urls = get_all_url(url)
-    for _url in urls:
-        ret = _url
-        if "freebuf.com" not in ret.replace('//', '').split('/')[0]:
-            remote_urls.append(_url)
-    return remote_urls
-
-
-def __main__():
-    url = 'http://freebuf.com/'
-    rurls = get_remote_urls(url)
-    print "--------------------remote urls-----------------------"
-    f=open('get_remote_urls.log','w')
-    for ret in rurls:
-        print ret
-#        f.write(ret)
-    f.close()
-
-    print "---------------------localurls-----------------------"
-    urls = get_local_urls(url)
-    f = open('get_local_urls.log', 'w')
-    for ret in urls:
-        print ret
- #       f.write(ret)
-    f.close()
-
-if __name__ == '__main__':
-    __main__()
+    myPageEyes = PageEyes()
+    myPageEyes.capture(url, capture_img)
