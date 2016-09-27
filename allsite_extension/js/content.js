010c8af8057cb3dd6c14473e1ec6a73935525919
songyq0509@163.com <syq314159>
2016-09-27 17:30:56 +0800

    Merge remote-tracking branch 'origin/master'

--- allsite_extension/js/content.js
+++ allsite_extension/js/content.js
@@ -1,15 +1,32 @@
 var patrn_rubbish = /uid|username|space|search|blog|group/;
 var patrn_detail = /post|thread|detail/;
 var patrn_list = /list|index|forum|fid/;
-$("a").each(function () {
-    var link = $(this).attr("href");
-    if (patrn_rubbish.exec(link)) {
-        $(this).css({"border-style": "solid", "border-color": "orange", "color": "orange"});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    var list_regexs = request.list_regexs;
+    var detail_regexs = request.detail_regexs;
+
+    console.log("recv:" + request.status);
+
+    for (var i in list_regexs) {
+        console.log(list_regexs[i]);
     }
-    if (patrn_detail.exec(link)) {
-        $(this).css({"border-style": "solid", "border-color": "green", "color": "green"});
+    for (var i in detail_regexs) {
+        console.log(detail_regexs[i]);
     }
-    if (patrn_list.exec(link)) {
-        $(this).css({"border-style": "solid", "border-color": "blue", "color": "blue"});
+    
+    if (request.status == "change") {
+        $("a").each(function() {
+            var link = $(this).attr("href");
+            if (patrn_rubbish.exec(link)) {
+                $(this).css({ "border-style": "solid", "border-color": "orange", "color": "orange" });
+            }
+            if (patrn_detail.exec(link)) {
+                $(this).css({ "border-style": "solid", "border-color": "green", "color": "green" });
+            }
+            if (patrn_list.exec(link)) {
+                $(this).css({ "border-style": "solid", "border-color": "blue", "color": "blue" });
+            }
+        });
     }
 });
---
+++ allsite_extension/js/popup.js
@@ -0,0 +1,59 @@
+var list_regexs;
+var detail_regexs;
+
+function httpRequest(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            callback(xhr.responseText);
+        }
+    };
+    xhr.send();
+}
+
+function showRegexs(result) {
+    result = JSON.parse(result);
+    list_regexs = JSON.parse(result).list_regexs;
+    detail_regexs = JSON.parse(result).detail_regexs;
+    // list_regexs= [{"regex":"list-","weight":"1"},{"regex":"index","weight":"1"},{"regex":"aaaa","weight":"1"}];
+    var table = '<table><tr><th>分类(列表/详情)</th><th>正则表达式</th><th>权重</th><th>采用</th></tr>';
+    for (var i in list_regexs) {
+        table += '<tr>';
+        table += '<td>列表</td>';
+        table += '<td><input type=text size=15 value=' + list_regexs[i].regex + '></td>';
+        table += '<td><input type=text size=2 value=' + list_regexs[i].weight + '></td>';
+        table += '<td>use</td>';
+        table += '</tr>';
+    }
+    for (var i in detail_regexs) {
+        table += '<tr>';
+        table += '<td>详情</td>';
+        table += '<td><input type=text size=15 value=' + detail_regexs[i].regex + '></td>';
+        table += '<td><input type=text size=2 value=' + detail_regexs[i].weight + '></td>';
+        table += '<td>use</td>';
+        table += '</tr>';
+    }
+    table += '</table>';
+    document.getElementById('regexs').innerHTML = table;
+
+    localStorage.list_regexs = list_regexs;
+    localStorage.detail_regexs = detail_regexs;
+    console.log(list_regexs);
+    console.log(detail_regexs);
+}
+
+function useFlgChange(e) {
+    console.log('send...');
+    var message = { status: "change", list_regexs: list_regexs, detail_regexs: detail_regexs };
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        for (var i = tabs.length - 1; i >= 0; i--) {
+            chrome.tabs.sendMessage(tabs[0].id, message);
+        }
+    });
+}
+
+var url = 'http://172.16.5.152:5000/regexs/list';
+httpRequest(url, showRegexs);
+
+document.body.onchange = useFlgChange;
