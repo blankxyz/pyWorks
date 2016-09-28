949ab106cd339569abe4e01d64592d9eb3b85401
songyq0509@163.com <syq314159>
2016-09-28 22:45:48 +0800

    Merge remote-tracking branch 'origin/master'

--- allsite_extension/js/content.js
+++ allsite_extension/js/content.js
@@ -1,23 +1,24 @@
-var patrn_rubbish;
-var patrn_detail;
-var patrn_list;
+var g_detail = new Array();
+var g_list = new Array();
 
 function is_list(url, rubbish_regexs, list_regexs, detail_regexs) {
 
 }
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     var regexs_list = new Array();
     var regexs_detail = new Array();
 
     console.log("request.opt:" + request.opt);
 
-    for (var i in request.list_regexs) {
-        regexs_list.push(request.list_regexs[i].regex)
+    for (var i = 0; i < request.list_regexs.length; i++) {
+        regexs_list.push(request.list_regexs[i].regex);
+        g_list.push(request.list_regexs[i]);
         // console.log(request.list_regexs[i].regex);
     }
-    for (var i in request.detail_regexs) {
-        regexs_detail.push(request.detail_regexs[i].regex)
+    for (var i = 0; i < request.detail_regexs.length; i++) {
+        regexs_detail.push(request.detail_regexs[i].regex);
+        g_detail.push(request.detail_regexs[i]);
         // console.log(request.detail_regexs[i].regex);
     }
 
@@ -28,19 +29,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
     if (request.opt == "change") {
         //window.location.reload();
-        $("a").each(function() {
+        $("a").each(function () {
             var link = $(this).attr("href");
-            $(this).css({ "border-style": "solid", "border-color": "black" });
+            $(this).css({"border-style": "dashed", "border-color": "black"});
             //if (patrn_rubbish.exec(link)) {
             //    $(this).css({ "border-style": "solid", "border-color": "orange" });
             //    //$(this).css({ "border-style": "solid", "border-color": "orange", "color": "orange" });
             //}
             if (patrn_detail.exec(link)) {
-                $(this).css({ "border-style": "solid", "border-color": "green" });
+                $(this).css({"border-style": "solid", "border-color": "green"});
                 //$(this).css({ "border-style": "solid", "border-color": "green", "color": "green" });
             }
             if (patrn_list.exec(link)) {
-                $(this).css({ "border-style": "solid", "border-color": "blue" });
+                $(this).css({"border-style": "solid", "border-color": "blue"});
                 //$(this).css({ "border-style": "solid", "border-color": "blue", "color": "blue" });
             }
         });
