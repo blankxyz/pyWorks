63e691eecbd8981887263ad94be5fe1bcff789ef
songyq0509@163.com <syq314159>
2016-10-27 20:59:51 +0800

    Merge remote-tracking branch 'origin/master'

--- youtube/subs.py
+++ youtube/subs.py
@@ -19,7 +19,6 @@ else:
     f.close()
     #  https://www.youtube.com/subscription_manager?action_takeout=1
     outline = opml.parse('subscription_manager.xml')
-
     ptime = datetime.utcfromtimestamp(float(content))
     ftime = time()
 
@@ -27,18 +26,18 @@ else:
 
     for i in range(0, len(outline[0])):
         urls.append(outline[0][i].xmlUrl)
-
     print(urls)
 
     videos = []
     for i in range(0, len(urls)):
         # print('Parsing through channel ' + str(i + 1) + ' out of ' + str(len(urls)), end='\\r')
         feed = feedparser.parse(urls[i])
-        print(feed)
+        print(feed) # URLError(error(10060, '')
         for j in range(0, len(feed['items'])):
             timef = feed['items'][j]['published_parsed']
             dt = datetime.fromtimestamp(mktime(timef))
             # if dt > ptime:
+            print(timef)
             videos.append(feed['items'][j]['link'])
 
     if len(videos) == 0:
@@ -48,6 +47,19 @@ else:
         print(videos)
 
     ydl_opts = {}
+    # ydl_opts = {
+    #     'format': 'bestaudio/best',
+    #     'extractaudio': True,
+    #     'audioformat': 'mp3',
+    #     'outtmpl': '%(id)s',  # name the file the ID of the video
+    #     'noplaylist': True,
+    #     'nocheckcertificate': True,
+    #     'postprocessors': [{
+    #         'key': 'FFmpegExtractAudio',
+    #         'preferredcodec': 'mp3',
+    #         'preferredquality': '192',
+    #     }]
+    # }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(videos)
