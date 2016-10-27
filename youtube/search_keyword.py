4c9bf9346f26d74867376a97e1c9e2f50f7d14c4
song <songyq0509@163.com>
2016-10-28 00:04:46 +0800

    Merge remote-tracking branch 'origin/master'

--- youtube/search_keyword.py
+++ youtube/search_keyword.py
@@ -3,26 +3,30 @@
 import re
 import urllib
 import redis
+import subprocess
+import youtube_dl
 
-REDIS_SERVER='redis://127.0.0.1/13'
+REDIS_SERVER = 'redis://127.0.0.1/13'
+
 
 ##################################################################################################
 class RedisDrive(object):
     def __init__(self):
-        self.site_domain='youtube.com'
+        self.site_domain = 'youtube.com'
         self.conn = redis.StrictRedis.from_url(REDIS_SERVER)
-        self.keyword_zset_key = 'keyword_zset_%s' % self.site_domain #状态管理
-        self.json_hset_key = 'json_hset_%s' % self.site_domain       #--print-json 结果保存
-        self.result_hset_key = 'result_hset_%s' % self.site_domain   # 最终结果保存
+        self.keyword_zset_key = 'keyword_zset_%s' % self.site_domain  # 状态管理
+        self.json_hset_key = 'json_hset_%s' % self.site_domain  # --print-json 结果保存
+        self.result_hset_key = 'result_hset_%s' % self.site_domain  # 最终结果保存
         self.todo_flg = -1
         self.videoId_flg = 0
         self.getJson_flg = 1
 
-    def add_video_id(self,video_id):
-        return self.conn.zadd(self.keyword_zset_key,self.todo_flg,video_id)
+    def add_video_id(self, video_id):
+        return self.conn.zadd(self.keyword_zset_key, self.todo_flg, video_id)
 
     def get_todo_videos(self):
-        return self.conn.zrangebyscore(self.keyword_zset_key,self.todo_flg,self.todo_flg,withscores=False)
+        return self.conn.zrangebyscore(self.keyword_zset_key, self.todo_flg, self.todo_flg, withscores=False)
+
 
 class KeywordSearch(object):
     def getHtml(self, url):
@@ -42,10 +46,15 @@ class KeywordSearch(object):
             redis_db.add_video_id(url)
             # result = (format % url)
             # f.write(result)
-        # f.close()
+            # f.close()
+
     def getJsonInfo(self):
         redis_db = RedisDrive()
         video_id_list = redis_db.get_todo_videos()
+        for video_id in video_id_list:
+            subprocess.Popen("youtube-dl", )
+            # p = subprocess.Popen(['/bin/bash', '-c', cmd], stdout=subprocess.PIPE)
+            # server_log_list = p.stdout.readlines()
 
 
 ##########################################################################################
@@ -59,6 +68,35 @@ def main():
         keySearch.getVideoId(html)
         i += 1
 
+
 ##########################################################################################
 if __name__ == '__main__':
-    main()
+    # main()
+    # ydl_opts = {
+    #     'format': 'bestaudio/best',
+    #     'extractaudio': True,
+    #     'audioformat': 'mp3',
+    #     'outtmpl': '%(id)s',  # name the file the ID of the video
+    #     'noplaylist': True,
+    #     'nocheckcertificate': True,
+    #     'postprocessors': [{
+    #         'key': 'FFmpegExtractAudio',
+    #         'preferredcodec': 'mp3',
+    #         'preferredquality': '192',
+    #     }]
+    # }
+    ydl_opts = {
+        'audioformat': 'mp3',
+        'outtmpl': '%(id)s',  # name the file the ID of the video
+        'noplaylist': True,
+        'nocheckcertificate': True,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }]
+    }
+
+    info_file = 'search_keyword.log'
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download_with_info_file(video_id, info_file)
