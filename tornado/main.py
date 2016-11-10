a0d562556197dbead4d07847e17d53349e4954f4
songyq0509@163.com <syq314159>
2016-11-10 17:20:40 +0800

    Merge remote-tracking branch 'origin/master'

--- tornado/main.py
+++ tornado/main.py
@@ -2,19 +2,43 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-import tornado
+
+import pymongo
 
 from tornado.options import define, options
 define("port", default=8000, help="run on the given port", type=int)
 
-class IndexHandler(tornado.web.RequestHandler):
-    def get(self):
-        greeting = self.get_argument('greeting', 'Hello')
-        self.write(greeting + ', friendly user!')
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [(r"/(\w+)", WordHandler)]
+        conn = pymongo.MongoClient("localhost", 27017)
+        self.db = conn["definitions"]
+        tornado.web.Application.__init__(self, handlers, debug=True)
+
+class WordHandler(tornado.web.RequestHandler):
+    def get(self, word):
+        coll = self.application.db.words
+        word_doc = coll.find_one({"word": word})
+        if word_doc:
+            del word_doc["_id"]
+            self.write(word_doc)
+        else:
+            self.set_status(404)
+    def post(self, word):
+        definition = self.get_argument("definition")
+        coll = self.application.db.words
+        word_doc = coll.find_one({"word": word})
+        if word_doc:
+            word_doc['definition'] = definition
+            coll.save(word_doc)
+        else:
+            word_doc = {'word': word, 'definition': definition}
+            coll.insert(word_doc)
+        del word_doc["_id"]
+        self.write(word_doc)
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
-    http_server = tornado.httpserver.HTTPServer(app)
+    http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
