21b51f04b29446b1e62469518cfaa3bec5595e1a
songyq0509@163.com <syq314159>
2016-11-20 17:59:42 +0800

    Merge remote-tracking branch 'origin/master'

--- tornado/move_rdis.py
+++ tornado/move_rdis.py
@@ -2,17 +2,18 @@
 # coding=utf-8
 
 import redis
+import json
 import time
 import datetime
 
-REDIS_SERVER = 'redis://127.0.0.1/11'
+REDIS_SERVER = 'redis://192.168.187.56/0'
 
 
 ##################################################################################################
 class WinxinBackup(object):
     def __init__(self):
         self.conn = redis.StrictRedis.from_url(REDIS_SERVER)
-        self.weixin_info_hset_key = '%hash_weixin_sns'
+        self.weixin_info_hset_key = 'hash_weixin_snsinfo'
 
     def get_weixin_info(self):
         return self.conn.hgetall(self.weixin_info_hset_key)
@@ -24,7 +25,12 @@ class WinxinBackup(object):
 def export():
     backup = WinxinBackup()
     print backup.get_weixin_cnt()
-
+    info_list = backup.get_weixin_info()
+    #for k,v in info_list.items():
+    j = json.dumps(info_list)
+    fd = open('./weixin.json','w')
+    fd.write(j)
+    fd.close()
 
 if __name__ == '__main__':
     export()
