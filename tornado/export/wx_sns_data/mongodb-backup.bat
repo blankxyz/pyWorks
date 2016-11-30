68f0c6ea1392a79199fdae300d428b23aeb3a650
songyq0509@163.com <syq314159>
2016-11-30 21:15:45 +0800

    Merge remote-tracking branch 'origin/master'

--- tornado/export/wx_sns_data/mongodb-backup.bat
+++ tornado/export/wx_sns_data/mongodb-backup.bat
@@ -1,3 +1,5 @@
-mongodump.exe -h 192.168.187.4:37017 -d wx_sns_data -o d:\MongoDB\backup
+mongodump.exe -h 192.168.187.4:37017 -d wx_sns_data -o D:\workspace\pyWorks\tornado\export\
 
-mongorestore.exe /h 127.0.0.1 /d wx_sns_data /dir d:\MongoDB\backup\wx_sns_data
+mongorestore.exe /h 127.0.0.1 /d wx_sns_data /dir D:\workspace\pyWorks\tornado\export\wx_sns_data
+
+pause
