42b052c2519b57bf6f086632536be58bb7184e9a
songyq0509@163.com <syq314159>
2017-01-10 18:49:58 +0800

    Merge remote-tracking branch 'origin/master'

--- django2/allsite/views.py
+++ django2/allsite/views.py
@@ -7,14 +7,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django import forms
 from models import User
+from pprint import pprint
 
 
 class UserForm(forms.Form):
-    username = forms.CharField(label='用户名', max_length=50)
-    password = forms.CharField(label='密码', widget=forms.PasswordInput())
+    username = forms.CharField(label='username', max_length=50)
+    password = forms.CharField(label='password', widget=forms.PasswordInput())
 
 
 def regist(req):
+    print '[info] regist start.'
     if req.method == 'POST':
         uf = UserForm(req.POST)
         if uf.is_valid():
@@ -23,22 +25,25 @@ def regist(req):
             password = uf.cleaned_data['password']
             # 添加到数据库
             User.objects.create(username=username, password=password)
-            return HttpResponse('regist success!!')
+            # return HttpResponse('regist success!!')
+            return HttpResponseRedirect('/allsite/login/')
     else:
         uf = UserForm()
+
+    print '[info] regist end.'
     return render_to_response('regist.html', {'uf': uf}, context_instance=RequestContext(req))
 
 
 def login(req):
+    print '[info] login start.'
     if req.method == 'POST':
         uf = UserForm(req.POST)
         if uf.is_valid():
-            # 获取表单用户密码
             username = uf.cleaned_data['username']
             password = uf.cleaned_data['password']
+            print '[info] login username: %s, password: %s' % (username, password)
             user = User.objects.filter(username__exact=username, password__exact=password)
             if user:
-                # 比较成功，跳转index
                 response = HttpResponseRedirect('/allsite/index/')
                 # 将username写入浏览器cookie,失效时间为3600
                 response.set_cookie('username', username, 60)
@@ -47,6 +52,8 @@ def login(req):
                 return HttpResponseRedirect('/allsite/login/')
     else:
         uf = UserForm()
+
+    print '[info] login end.'
     return render_to_response('login.html', {'uf': uf}, context_instance=RequestContext(req))
 
 
